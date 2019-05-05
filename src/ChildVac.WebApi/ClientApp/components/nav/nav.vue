af1c9f5bccb15c51d21e023a1a97061960f5f99c
Batyr Seven <batyrseven@gmail.com>
2019-05-05 17:58:48 +0600

    Restyled nav buttons v3

--- src/ChildVac.WebApi/ClientApp/components/nav/nav.vue
+++ src/ChildVac.WebApi/ClientApp/components/nav/nav.vue
@@ -3,19 +3,27 @@
         <div class="nav_button-wrapper pt-3 pb-1">
             <b-button to="/calendar" :variant="getButtonVariant('calendar')" class="nav_button">
                 <font-awesome-icon icon="calendar-week" size="2x" />
-                <p class="mt-2 mb-0">Календарь</p>
+                <p class="mt-2 mb-0 nav_text-wrapper">
+                    <span class="nav_text">Календарь</span>
+                </p>
             </b-button>
             <b-button to="/ticket" :variant="getButtonVariant('ticket')" class="nav_button">
                 <font-awesome-icon icon="pen" size="2x" />
-                <p class="mt-2 mb-0">Запись на прием</p>
+                <p class="mt-2 mb-0 nav_text-wrapper">
+                    <span class="nav_text">Запись на прием</span>
+                </p>
             </b-button>
             <b-button to="/register-child" :variant="getButtonVariant('register-child')" class="nav_button">
                 <font-awesome-icon icon="baby" size="2x" />
-                <p class="mt-2 mb-0">Регистрация ребенка</p>
+                <p class="mt-2 mb-0 nav_text-wrapper">
+                    <span class="nav_text">Регистрация ребенка</span>
+                </p>
             </b-button>
             <b-button to="/register-parent" :variant="getButtonVariant('register-parent')" class="nav_button">
                 <font-awesome-icon icon="user-plus" size="2x" />
-                <p class="mt-2 mb-0">Регистрация родителя</p>
+                <p class="mt-2 mb-0 nav_text-wrapper">
+                    <span class="nav_text">Регистрация родителя</span>
+                </p>
             </b-button>
         </div>
     </div>
